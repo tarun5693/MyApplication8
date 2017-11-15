37deef3458b0d2e1e8d78400c05a07fd52912652
tarun5693 <tanujangid2.tj@gmail.com>
2017-11-15 19:03:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tarun/myapplication/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.example.tarun.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+    public void onBackPressed(){
+        finish();
+        //Toast.makeText(this, "on main page", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/tarun/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.tarun.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Timer timer=new Timer();
+        timer.schedule(new TimerTask(){
+            public void run() {
+                Intent i=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+                finish();
+            }
+        },4000);
+    }
+}
